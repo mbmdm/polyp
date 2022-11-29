e44ed4f0ed64f1ca7414f8fedfe694ac8d46f7a0
Артем Анисимов <mbmdm@bk.ru>
2022-11-29 22:43:10 +0300

    Add GLM submodule

--- example/main.cpp
+++ example/main.cpp
@@ -3,6 +3,10 @@
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 int main(void)
 {
     GLFWwindow* window;
