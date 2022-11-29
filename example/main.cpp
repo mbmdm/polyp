a90c429c2af28600dd6155a5d3fe071af4732620
Artem Anisimov <38132733+mbmdm@users.noreply.github.com>
2022-11-29 23:05:09 +0300

    Add submodules STB and GLM (#4)

--- example/main.cpp
+++ example/main.cpp
@@ -3,6 +3,12 @@
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <stb_image.h>
+
 int main(void)
 {
     GLFWwindow* window;
