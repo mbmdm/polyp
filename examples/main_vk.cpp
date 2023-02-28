5e8e6298f395be35c9d6575fa768be0f3885d688
Your Name <you@example.com>
2023-02-28 22:42:16 +0300

    A bit redid examples targets

---
+++ examples/main_vk.cpp
@@ -0,0 +1,10 @@
+#include <camera.h>
+#include <utils_errors.h>
+//#include <shader.h>
+#include <device.h>
+#include <instance.h>
+
+int main(void) {
+    polyp::engine::Instance inst;
+    inst.init();
+}
