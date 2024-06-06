fcc87efb9e176271209d0db5a1426225a00f5ff1
mbmdm <mbmdm@bk.ru>
2024-06-06 20:40:29 +0300

    New sample 3D box

---
+++ samples/simple_box/simple_box.cpp
@@ -0,0 +1,124 @@
+#include <example_basic_pipeline.h>
+
+using namespace polyp;
+
+using ShaderModule = polyp::vulkan::ShaderModule;
+
+class SimpleBox : public example::ExampleBasicPipeline
+{
+protected:
+    ShadersData loadShaders() override
+    {
+        auto vert  = vulkan::utils::loadSPIRV("shaders/simple_triangle/simple_triangle.vert.spv");
+        auto index = vulkan::utils::loadSPIRV("shaders/simple_triangle/simple_triangle.frag.spv");
+        
+        return std::make_tuple(std::move(vert), std::move(index));
+    }
+
+    ModelsData loadModel() override
+    {
+        float vertices[] =
+        {
+            -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, 0.0f,
+             0.5f, -0.5f, -0.5f,  0.5f, 0.0f, 0.0f,
+             0.5f,  0.5f, -0.5f,  0.5f, 1.0f, 0.0f,
+             0.5f,  0.5f, -0.5f,  0.5f, 1.0f, 0.0f,
+            -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 0.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, 0.0f,
+    
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 0.0f,
+             0.5f, -0.5f,  0.5f,  0.5f, 0.0f, 0.0f,
+             0.5f,  0.5f,  0.5f,  0.5f, 1.0f, 0.0f,
+             0.5f,  0.5f,  0.5f,  0.5f, 1.0f, 0.0f,
+            -0.5f,  0.5f,  0.5f,  0.0f, 1.0f, 0.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 0.0f,
+    
+            -0.5f,  0.5f,  0.5f,  0.5f, 0.0f, 0.0f,
+            -0.5f,  0.5f, -0.5f,  0.5f, 1.0f, 0.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 0.0f,
+            -0.5f,  0.5f,  0.5f,  0.5f, 0.0f, 0.0f,
+    
+             0.5f,  0.5f,  0.5f,  0.5f, 0.0f, 0.0f,
+             0.5f,  0.5f, -0.5f,  0.5f, 1.0f, 0.0f,
+             0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.0f,
+             0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.0f,
+             0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 0.0f,
+             0.5f,  0.5f,  0.5f,  0.5f, 0.0f, 0.0f,
+    
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.0f,
+             0.5f, -0.5f, -0.5f,  0.5f, 1.0f, 0.0f,
+             0.5f, -0.5f,  0.5f,  0.5f, 0.0f, 0.0f,
+             0.5f, -0.5f,  0.5f,  0.5f, 0.0f, 0.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 0.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.0f,
+    
+            -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 0.0f,
+             0.5f,  0.5f, -0.5f,  0.5f, 1.0f, 0.0f,
+             0.5f,  0.5f,  0.5f,  0.5f, 0.0f, 0.0f,
+             0.5f,  0.5f,  0.5f,  0.5f, 0.0f, 0.0f,
+            -0.5f,  0.5f,  0.5f,  0.0f, 0.0f, 0.0f,
+            -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 0.0f
+        };
+    
+        std::vector<Vertex> vertexData(sizeof(vertices) / (sizeof(float) * 6));
+    
+        for (size_t i = 0; i < vertexData.size(); ++i)
+        {
+            size_t j = i * 6;
+    
+            vertexData[i].position[0] = vertices[j];
+            vertexData[i].position[1] = vertices[j + 1];
+            vertexData[i].position[2] = vertices[j + 2];
+    
+            j += 3;
+    
+            vertexData[i].color[0] = vertices[j];
+            vertexData[i].color[1] = vertices[j + 1];
+            vertexData[i].color[2] = vertices[j + 2];
+        }
+    
+        std::vector<uint32_t> indexData(vertexData.size());
+    
+        std::iota(indexData.begin(), indexData.end(), 0);
+    
+        return std::make_tuple(std::move(vertexData), std::move(indexData));
+    }
+
+    UniformData loadUniformData() override
+    {
+        glm::mat4 view       = glm::mat4(1.0f);
+        glm::mat4 model      = glm::mat4(1.0f);
+        glm::mat4 projection = glm::mat4(1.0f);
+
+        auto& ctx = vulkan::RHIContext::get();
+        auto capabilities = ctx.gpu().getSurfaceCapabilitiesKHR(*ctx.surface());
+
+        auto width  = capabilities.currentExtent.width;
+        auto height = capabilities.currentExtent.height;
+
+        model      = glm::rotate(model, glm::radians(-30.0f), glm::vec3(1.0f, 1.0f, 0.0f));
+        view       = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        projection = glm::perspective(glm::radians(45.0f), (float)width / (float)height, 0.1f, 100.0f);
+
+        return UniformData{ projection, model, view };
+    }
+};
+
+int main()
+{
+    std::string title{ constants::kWindowTitle };
+    title += ": simple box";
+
+    SimpleBox sample{};
+
+    Application::get().onWindowInitialized += [&sample](const auto& args) {sample.onInit(args); };
+    Application::get().onWindowResized     += [&sample](const auto& args) {sample.onResize(); };
+    Application::get().onFrameRender       += [&sample]() {sample.draw(); };
+
+    Application::get().init(title.c_str(), 1024, 600);
+    Application::get().run();
+
+    return EXIT_SUCCESS;
+}
